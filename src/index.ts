d78ca00c40a1ccd64def50064ea286f0191cbec0
Konrad Korus <konrad.korus@flip.shop>
2026-06-05 17:17:36 +0200

    Update dependencies to latest versions, modify package scripts for development, and adjust TypeScript configuration for output directory. Refactor index.ts to improve logging delay and clean up imports.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { DevTools } from '@effect/experimental';
-import { NodeRuntime, NodeSocket } from '@effect/platform-node';
-import { Effect, Layer } from 'effect';
+import { NodeRuntime } from '@effect/platform-node';
+import { Effect } from 'effect';
 
 const program = Effect.log('Hello!').pipe(
-  Effect.delay(2000),
+  Effect.delay('2 seconds'),
   Effect.withSpan('Hi', { attributes: { foo: 'bar' } }),
   Effect.forever,
 );
+
 const DevToolsLive = DevTools.layer();
 
 program.pipe(Effect.provide(DevToolsLive), NodeRuntime.runMain);
